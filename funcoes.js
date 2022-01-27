f0faba8805427933e6fa26b1a5ad24a8870eea90
Tio Geleia <stochinifelippe@gmail.com>
2022-01-27 10:29:50 -0300

    arrumei menu responsivo

--- funcoes.js
+++ funcoes.js
@@ -5,3 +5,13 @@ function mudafoto(foto) {
 function amplia(amp) {
     document.getElementById("onibus").src = amp;
 }
+
+function fechar() {
+
+    var inputCheckbox = document.getElementById('check');
+
+    if (inputCheckbox.checked === true) {
+        inputCheckbox.checked = false;
+    }
+
+}
